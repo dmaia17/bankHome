c8df72278222f3d5e0ebc5256f5e116113d430bb
danielbs2 <daniel.passos@bs2tecnologia.com.br>
2023-04-04 11:04:51 -0300

    add initial commits

---
+++ BankHome/Pods/Target Support Files/Pods-BankHome/Pods-BankHome-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BankHome : NSObject
+@end
+@implementation PodsDummy_Pods_BankHome
+@end
---
+++ BankHome/Pods/Target Support Files/Pods-BankHomeTests/Pods-BankHomeTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BankHomeTests : NSObject
+@end
+@implementation PodsDummy_Pods_BankHomeTests
+@end
