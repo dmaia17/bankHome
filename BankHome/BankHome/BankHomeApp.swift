9da36747edda2bf1e651e298d2ed9d6b4a860387
danielbs2 <daniel.passos@bs2tecnologia.com.br>
2023-04-04 10:49:51 -0300

    add initial files

---
+++ BankHome/BankHome/BankHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankHomeApp.swift
+//  BankHome
+//
+//  Created by Daniel Maia dos Passos on 04/04/23.
+//
+
+import SwiftUI
+
+@main
+struct BankHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BankHome/BankHomeUITests/BankHomeUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BankHomeUITestsLaunchTests.swift
+//  BankHomeUITests
+//
+//  Created by Daniel Maia dos Passos on 04/04/23.
+//
+
+import XCTest
+
+final class BankHomeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
